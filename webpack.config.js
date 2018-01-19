9110ac50e04636e3e3f97087e7f5ef1cd815beb5
Filippo Conti <filippo@codekraft.it>
2018-01-19 12:05:43 +0100

    Multiple target config Support for node and web module exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-module.exports = {
-  entry: './lib/index.js',
-  output: {
-    library: 'Censys',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    filename: './build/censys.js'
+module.exports = [
+  {
+    entry: './lib/index.js',
+    target: 'web',
+    output: {
+      library: 'Censys',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      filename: './build/censys.web.js'
+    }
+  },
+  {
+    entry: './lib/index.js',
+    target: 'node',
+    output: {
+      library: 'Censys',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      filename: './build/censys.js'
+    }
   }
-};
+];
